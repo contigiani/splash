7264a0fb498e948f23e459e03c8913305c7fe106
Omar Contigiani <omarcontigiani@Omars-MacBook-Pro.local>
2018-01-04 19:21:31 +0100

    Fix stack.py

--- stack.py
+++ stack.py
@@ -3,8 +3,7 @@ from astropy import units as u
 import numpy as np
 
 CCCP = cluster_sample('data/CCCP.fits', 'data/source/')
-#bin_edges = np.array([10,30,60,90,120,150,180,240,300,360,420,480,540,600,720,840,960,1080,1300, 1600])*u.arcsec
-bin_edges = np.geomspace(.1, 7, 32)*u.Mpc
+bin_edges = np.geomspace(.5, 5, 28)*u.Mpc
 CCCP.stack_ESD(bin_edges)
 
 from matplotlib import pyplot as plt
@@ -22,5 +21,17 @@ plt.ylabel(r'$\Delta \Sigma (R)$ ($h^{-1}$ '+str(CCCP.ESDs.unit*u.rad)+')')
 plt.gca().set_xscale('log')
 plt.gca().set_yscale('log')
 plt.gca().set_xlim([bin_edges[0].value, bin_edges[-1].value])
+plt.gca().set_ylim([1e-15, 1e-12])
 plt.suptitle('Stacked profile')
-plt.savefig('stack.pdf')
+plt.savefig('output/stack.pdf')
+
+for i in xrange(CCCP.size):
+    plt.clf()
+    plt.errorbar(CCCP.stack_rbin.value, (CCCP.ESDs[i]).value,yerr=(CCCP.ESDs_err[i]).value, c='0.5', zorder=100)
+    plt.xlabel(r'$R$ $(h^{-1} Mpc)$')
+    plt.ylabel(r'$\Delta \Sigma (R)$ ($h^{-1}$ '+str(CCCP.ESDs.unit*u.rad)+')')
+    plt.gca().set_xscale('log')
+    plt.gca().set_yscale('log')
+    plt.gca().set_xlim([bin_edges[0].value, bin_edges[-1].value])
+    plt.suptitle(CCCP[i].name+'profile')
+    plt.savefig('output/individual/'+CCCP[i].name+'.pdf')
