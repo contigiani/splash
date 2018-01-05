Fix units in stack.py and cluster.py

cluster.py
@@ -68,7 +68,7 @@ class cluster_sample:
             idxlist : list
                 Indices of the array to stack. Defaults to stacking all clusters.
         '''
-
+        import astropy.constants as const
         if(idxlist == None):
             idxlist = range(self.size)
 
@@ -78,11 +78,11 @@ class cluster_sample:
         else:
             [self.clusters[i].compute_shear(bin_edges) for i in xrange(self.size)]
 
-        self.ESDs, self.ESDs_err = np.zeros([self.size, len(bin_edges)-1])/u.Mpc/u.Msun*u.rad, np.zeros([self.size, len(bin_edges)-1])/u.Mpc/u.Msun*u.rad
+        self.ESDs, self.ESDs_err = np.zeros([self.size, len(bin_edges)-1])/u.Mpc/u.Mpc, np.zeros([self.size, len(bin_edges)-1])/u.Mpc/u.Mpc
         for i in xrange(self.size):
             if(i in idxlist):
-                self.ESDs[i] = self.clusters[i].gtbin/self.da[i]/self.beta_avg[i]/self.m_500[i]
-                self.ESDs_err[i] = self.clusters[i].dgtbin/self.da[i]/self.beta_avg[i]/self.m_500[i]
+                self.ESDs[i] = self.clusters[i].gtbin/self.da[i]/self.beta_avg[i]/self.m_500[i]*(const.c**2.)/4./np.pi/const.G/u.rad
+                self.ESDs_err[i] = self.clusters[i].dgtbin/self.da[i]/self.beta_avg[i]/self.m_500[i]*(const.c**2.)/4./np.pi/const.G/u.rad
 
         rmin = bin_edges[:-1]
         rmax = bin_edges[1:]

stack.py
@@ -3,8 +3,9 @@ from astropy import units as u
 import numpy as np
 from matplotlib import pyplot as plt
 
-CCCP = cluster_sample('data/CCCP.fits', 'data/source/')
+CCCP = cluster_sample('data/CCCPhighz.fits', 'data/source/')
 bin_edges = np.geomspace(.5, 7, 32)*u.Mpc
+#bin_edges = np.geomspace(.5, 5., 19)*u.Mpc
 CCCP.stack_ESD(bin_edges)
 
 plt.errorbar(CCCP.stack_rbin.value, (CCCP.stack_ESD).value, yerr=(CCCP.stack_ESDerr).value, ecolor='#dd0f0f', color='#dd0f0f', lw=4, zorder=100, fmt='o', mew=0)
@@ -13,10 +14,10 @@ ESDs = CCCP.ESDs
 ESDs[ESDs<=0] = np.nan
 
 for i in xrange(CCCP.size):
-    plt.errorbar(CCCP.stack_rbin.value, CCCP.stack_rbin.value*(ESDs[i]).value, c='0.5', zorder=1)# yerr=(CCCP.ESDs_err[i]).value)
+    plt.errorbar(CCCP.stack_rbin.value, (ESDs[i]).value, c='0.5', zorder=1)
 
 plt.xlabel(r'$R$ $(h^{-1} Mpc)$')
-plt.ylabel(r'$\Delta \Sigma (R)$ ($h^{-1}$ '+str(CCCP.ESDs.unit/u.rad)+')')
+plt.ylabel(r' $\Delta \Sigma (R)/M_{500}$ ($h^{-2}$ '+str(CCCP.ESDs.unit)+')')
 plt.gca().set_xscale('log')
 plt.gca().set_yscale('log')
 plt.gca().set_xlim([bin_edges[0].value, bin_edges[-1].value])
@@ -28,7 +29,7 @@ for i in xrange(CCCP.size):
     plt.clf()
     plt.errorbar(CCCP.stack_rbin.value, (CCCP.ESDs[i]).value,yerr=(CCCP.ESDs_err[i]).value, c='0.5', zorder=100)
     plt.xlabel(r'$R$ $(h^{-1} Mpc)$')
-    plt.ylabel(r'Rescaled $\Delta \Sigma (R)$ ($h^{-1}$ '+str(CCCP.ESDs.unit/u.rad)+')')
+    plt.ylabel(r' $\Delta \Sigma (R)/M_{500}$ ($h^{-2}$ '+str(CCCP.ESDs.unit)+')')
     plt.gca().set_xscale('log')
     plt.gca().set_yscale('log')
     plt.gca().set_xlim([bin_edges[0].value, bin_edges[-1].value])